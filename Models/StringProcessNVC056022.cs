835ee25620e287abca3974c460f27e89ebfef38e
Nguyen Van Cuong <1521050560@student.humg.edu.vn>
2022-03-23 00:00:23 +0700

    Create_Class_StrPro_NVC56022

---
+++ Models/StringProcessNVC056022.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace NguyenVanCuong56022
+{
+    public class StringProcessNVC056022
+    {
+        public string UpWord (string strInput)
+        {
+            string chuoi = "";
+            chuoi = strInput.ToUpper();
+            return chuoi;
+        }
+    }
+}
